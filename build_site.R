7b4cfc874d630a71c7ca88c5fda8c8b7663a73f0
sonusharma07 <meetsonusharma07@gmail.com>
2018-07-04 11:16:46 +0530

    first website commit

---
+++ build_site.R
@@ -0,0 +1,8 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("C:/Users/Chandan Sharma/Documents/my_site")
+
+#render your sweet site. 
+rmarkdown::render_site()
